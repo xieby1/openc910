846b1418c825502900609c7a8320a3bd68a7436f
xieby1 <xieby1@outlook.com>
2024-05-23 22:49:58 +0800

    pin nixpkgs to latest 23.11

--- shell.nix
+++ shell.nix
@@ -1,5 +1,11 @@
 let
-  pkgs = import <nixpkgs> {};
+  # pin latest nixpkgs 23.11
+  pkgs = import ( (import <nixpkgs> {}).fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "46397778ef1f73414b03ed553a3368f0e7e33c2f";
+    hash = "sha256-mzZDr00WUiUXVm1ujBVv6A0qRd8okaITyUp4ezYRgc4=";
+  } ) {};
 in pkgs.mkShell {
   packages = with pkgs; [
     verilator
