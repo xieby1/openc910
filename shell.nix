13f3bc33774e9f0d549ac7b5cb86da0aea73b25f
xieby1 <xieby1@outlook.com>
2024-05-30 20:36:47 +0800

    add h

--- shell.nix
+++ shell.nix
@@ -1,13 +1,29 @@
 let
   # pin latest nixpkgs 23.11
+  name = "openc910";
   pkgs = import ( (import <nixpkgs> {}).fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
     rev = "46397778ef1f73414b03ed553a3368f0e7e33c2f";
     hash = "sha256-mzZDr00WUiUXVm1ujBVv6A0qRd8okaITyUp4ezYRgc4=";
   } ) {};
+  h_content = builtins.toFile "h_content" ''
+    # ${pkgs.lib.toUpper "xieby1's ${name} usage tips"}
+
+    Branch `micro_translator` is the active main branch of transutor project.
+
+    * Show this help: `h`
+    * Build c910: ❶ `cd smart_run`
+      * help: ❷ `make help`
+      * coremark ❷ `make runcase CASE=coremark SIM=verilator THREADS=8 -j`
+    * Run: ❶ `cd smart_run/work` ❷ `./obj_dir/Vtop`
+  '';
+  _h_ = pkgs.writeShellScriptBin "h" ''
+    ${pkgs.glow}/bin/glow ${h_content}
+  '';
 in pkgs.mkShell {
   packages = with pkgs; [
+    _h_
     verilator
   ];
 
@@ -28,5 +44,7 @@ in pkgs.mkShell {
         echo ${xuantie-elf-toolchain} > $file
       fi
     fi
+
+    h
   '';
 }
